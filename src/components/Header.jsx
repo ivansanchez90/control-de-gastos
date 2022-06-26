c9bc56d7b01f9c34c91116a2dffe0e05c7324c18
ivansanchez90 <sanchezoliva.ivan@gmail.com>
2022-06-26 18:59:04 -0300

    agregando control de presupuesto y modal

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import ControlPresupuesto from "./ControlPresupuesto";
 import NuevoPresupuesto from "./NuevoPresupuesto";
 
 const Header = ({
@@ -11,7 +12,9 @@ const Header = ({
     <header>
       <h1>Planificador de Gastos</h1>
       {isValidPresupuesto ? (
-        <p>Control Presupuesto</p>
+        <ControlPresupuesto 
+          presupuesto={presupuesto}
+        />
       ) : (
         <NuevoPresupuesto
           presupuesto={presupuesto}
